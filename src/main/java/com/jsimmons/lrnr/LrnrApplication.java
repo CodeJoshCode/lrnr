9430dbaf5b6c7437541456238d381e80a8368392
Josh <simmonsj711@dupage.edu>
2021-01-03 13:47:27 -0600

    added startup files from spring initializer

---
+++ src/main/java/com/jsimmons/lrnr/LrnrApplication.java
@@ -0,0 +1,13 @@
+package com.jsimmons.lrnr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LrnrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LrnrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsimmons/lrnr/LrnrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsimmons.lrnr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LrnrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
