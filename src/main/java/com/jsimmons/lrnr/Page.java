b2295c27ae7c850b668c1b99d7e489eceee43a9d
Brandon Dehmer <brandond27@comcast.net>
2021-01-25 14:20:59 -0600

    commented out entity annotations for now here

--- src/main/java/com/jsimmons/lrnr/Page.java
+++ src/main/java/com/jsimmons/lrnr/Page.java
@@ -3,6 +3,6 @@ package com.jsimmons.lrnr;
 import javax.persistence.*;
 
 public abstract class Page {
-    private @Id @GeneratedValue Long id;
+    //private @Id @GeneratedValue Long id;
     //private String fileLocation;
 }
