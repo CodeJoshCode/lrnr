a48d16f7d821491b96a8e4016e3a06efe79ed008
Brandon Dehmer <brandond27@comcast.net>
2021-01-25 15:31:01 -0600

    Added services folder and Notebook Service class

---
+++ src/main/java/com/jsimmons/lrnr/Services/NotebookService.java
@@ -0,0 +1,22 @@
+package com.jsimmons.lrnr.Services;
+
+import com.jsimmons.lrnr.Entities.Notebook;
+import com.jsimmons.lrnr.repositories.NotebookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NotebookService {
+
+    @Autowired
+    NotebookRepository repository;
+
+    public List<Notebook> getNotebooks() {
+        return (List<Notebook>) repository.findAll();
+    }
+    public void saveNotebook(Notebook n) {
+        repository.save(n);
+    }
+}
