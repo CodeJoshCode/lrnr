61ed72ea92374a89e4c737e2ed48397684f675d3
Brandon Dehmer <brandond27@comcast.net>
2021-01-27 17:08:51 -0600

    started main page

---
+++ src/main/java/com/jsimmons/lrnr/Controllers/IndexController.java
@@ -0,0 +1,31 @@
+package com.jsimmons.lrnr.Controllers;
+
+import com.jsimmons.lrnr.Entities.Notebook;
+import com.jsimmons.lrnr.Services.NotebookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @Autowired
+    NotebookService notebookService;
+
+    @GetMapping
+    public String getIndex() {
+        return "index";
+    }
+    @PostMapping
+    public String postIndex(@RequestParam(name="notebook") String notebookPath, Model model) {
+        notebookService.saveNotebook(new Notebook(notebookPath));
+        model.addAttribute("notebook", notebookPath);
+        model.addAttribute("notebooks", notebookService.getNotebooks());
+        return "index";
+    }
+}
