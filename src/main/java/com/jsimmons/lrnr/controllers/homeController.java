e02b233b9bba00705af9766f8c5b3727226ce681
Josh Simmons <simmonsj711@dupage.edu>
2021-02-16 16:20:22 -0600

    error with adding pages

--- src/main/java/com/jsimmons/lrnr/controllers/homeController.java
+++ src/main/java/com/jsimmons/lrnr/controllers/homeController.java
@@ -1,6 +1,7 @@
 package com.jsimmons.lrnr.controllers;
 
 import com.jsimmons.lrnr.entities.Notebook;
+import com.jsimmons.lrnr.entities.Page;
 import com.jsimmons.lrnr.services.NotebookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -30,9 +31,21 @@ public class homeController {
 
     // pages for individual notebooks
 
-    @GetMapping("/usernotebook/{notebook_identifier}")
+    @GetMapping("/user_notebook/{notebook_identifier}")
     public String getUserNotebook(@PathVariable(value = "notebook_identifier") String notebookIdentifier, Model model) throws Exception {
         Notebook notebook = notebookService.findNotebookByName(notebookIdentifier);
+        model.addAttribute("user_notebook", notebook);
+        return "user_notebook";
+    }
+
+    @PostMapping("/user_notebook/{notebook_identifier}")
+    public String postUserNotebook(@PathVariable(value = "notebook_identifier") String notebookIdentifier,@RequestParam(name = "page_name") String pageName, Model model) throws Exception{
+        Notebook notebook = notebookService.findNotebookByName(notebookIdentifier);
+        Page new_page = new Page();
+        new_page.setName(pageName);
+        new_page.setNotebook(notebook);
+        notebook.getPages().add(new Page());
+        notebookService.saveNotebook(notebook);
         model.addAttribute("user_notebook", notebook);
         return "user_notebook";
     }
