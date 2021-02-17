c8c00866213bff461d18fe8db460c6f88c63a5fb
Brandon Dehmer <brandond27@comcast.net>
2021-02-17 11:52:49 -0600

    added Edit endpoint for Pages

--- src/main/java/com/jsimmons/lrnr/controllers/homeController.java
+++ src/main/java/com/jsimmons/lrnr/controllers/homeController.java
@@ -47,9 +47,11 @@ public class homeController {
     @PostMapping("/user_notebook/{notebook_identifier}")
     public String postUserNotebook(@PathVariable(value = "notebook_identifier") Long notebookIdentifier,@RequestParam(name = "page_name") String pageName, Model model) throws Exception{
         Notebook notebook = notebookService.findById(notebookIdentifier);
+        //TODO : this looks like it should be abstracted to a constructor below
         Page new_page = new Page();
         new_page.setName(pageName);
         new_page.setNotebook(notebook);
+        new_page.setTextContents("");
         ArrayList<Page> pageList = new ArrayList<Page>();
         pageList.addAll(notebook.getPages());
         pageList.add(new_page);
@@ -63,22 +65,63 @@ public class homeController {
     public String getUserPage(@PathVariable(value = "page_identifier") Long pageIdentifier, Model model ) {
         Page page =  pageService.findById(pageIdentifier);
         model.addAttribute("user_page", page);
+
         return "user_page";
     }
 
-    // TODO: change parameters to a map
+//    // TODO: change parameters to a map
+//    @PostMapping("/user_page/{page_identifier}")
+//    public String postUserPage(@PathVariable(value = "page_identifier") Long pageIdentifier,
+//                               @RequestParam(name = "id") Long page_id,
+//                               @RequestParam(name = "page_name") String page_name,
+//                               @RequestParam(name = "page_text_content") String page_contents,
+//                               Model model){
+//        Page page =  pageService.findById(pageIdentifier);
+//        page.setTextContents(page_contents);
+//        pageService.savePage(page);
+//        model.addAttribute("user_page", page);
+//        return "user_page";
+//    }
     @PostMapping("/user_page/{page_identifier}")
     public String postUserPage(@PathVariable(value = "page_identifier") Long pageIdentifier,
-                               @RequestParam(name = "id") Long page_id,
-                               @RequestParam(name = "page_name") String page_name,
+                               //@RequestParam(name = "page_name") String pageName,
                                @RequestParam(name = "page_text_content") String page_contents,
-                               Model model){
-        Page page =  pageService.findById(pageIdentifier);
+                               Model model) {
+        Page page = pageService.findById(pageIdentifier);
+        System.out.println(page);
         page.setTextContents(page_contents);
+        pageService.savePage(page);
+        model.addAttribute("user_page", page);
+        System.out.println(page);
+        return "user_page";
+    }
+
+    // edit pages
+
+    @GetMapping("/user_page/edit/{page_identifier}")
+    public String getUserPageEdit(@PathVariable(value = "page_identifier") Long pageIdentifier, Model model) {
+        Page page = pageService.findById(pageIdentifier);
+        model.addAttribute("user_page", page);
+
+        return "edit_user_page";
+    }
+
+    @PostMapping("/user_page/edit/{page_identifier}")
+    public String postUserPageEdit(@PathVariable(value = "page_identifier") Long pageIdentifier,
+                                   @RequestParam(name = "page_name", required = false) String pageName,
+                                   @RequestParam(name = "page_text_content", required = false) String pageContents,
+                                   Model model) {
+        //TODO : pretty sure theres specific spring validation for null checking these requestparams
+        Page page = pageService.findById(pageIdentifier);
+        if (pageName != null) {
+            page.setName(pageName);
+        }
+        if (pageContents != null) {
+            page.setTextContents(pageContents);
+        }
         pageService.savePage(page);
         model.addAttribute("user_page", page);
         return "user_page";
     }
-
 
 }
