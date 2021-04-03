b4d71e6710793df4ffa1bcec64986f7e91ec19f6
Brandon Dehmer <brandond27@comcast.net>
2021-04-02 23:13:05 -0500

    updated account confirmation from rest controller -> now works from webpage / form submission

--- src/main/java/com/jsimmons/lrnr/controllers/RegistrationController.java
+++ src/main/java/com/jsimmons/lrnr/controllers/RegistrationController.java
@@ -2,24 +2,41 @@ package com.jsimmons.lrnr.controllers;
 
 import com.jsimmons.lrnr.registration.RegistrationRequest;
 import com.jsimmons.lrnr.registration.RegistrationService;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 @RequestMapping(path = "api/v1/registration")
-@AllArgsConstructor
 public class RegistrationController {
+
+    Logger logger = LoggerFactory.getLogger(RegistrationController.class);
+
+    public RegistrationController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
 
     private final RegistrationService registrationService;
 
     @PostMapping
-    public String register(@RequestBody RegistrationRequest request) {
-        return registrationService.register(request);
+    public String register(RegistrationRequest request) {
+        logger.info("calling registration post method");
+        //this line returns the token that should be sent after clicking confirmation link from provided email
+        registrationService.register(request);
+        return "emailconfirmation";
     }
 
     @GetMapping(path = "confirm")
     public String confirm(@RequestParam("token") String token) {
-        return registrationService.confirmToken(token);
+        logger.info("calling registration confirmation");
+        if (!registrationService.confirmToken(token).equals("confirmed")) {
+            throw new IllegalStateException("token was unable to be confirmed");
+        }
+        return "confirmed";
     }
+
 }
---
+++ src/main/resources/js/redirect.js
@@ -0,0 +1,2 @@
+window.location.replace("login");
+console.log("u did it bro");
