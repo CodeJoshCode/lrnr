cf13df8d8be420a6d1c01eafd77aa5c81f66c123
Brandon Dehmer <brandond27@comcast.net>
2021-04-08 12:53:19 -0500

    added notes on @Transactional

--- src/main/java/com/jsimmons/lrnr/repositories/AppUserRepository.java
+++ src/main/java/com/jsimmons/lrnr/repositories/AppUserRepository.java
@@ -17,6 +17,21 @@ public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 
     Optional<AppUser> findByEmail(String email);
 
+    /*
+    ON @Transactional:
+
+    Jpa itself does not provide any kind of declarative transaction management (it just does magic on your @entity obj's)
+    When using jpa outside of a dependency injection container i.e. springs ioc container, transactions need to be handled
+    programmatically by the developer.
+
+    it looks like @Transactional abstracts having to create a db connection, carrying out a single db transaction,
+    and then finally closing that connection.
+
+     */
+
+    //TODO : this probably belongs in the service layer... see:  https://stackoverflow.com/questions/1079114/where-does-the-transactional-annotation-belong
+
+    // this query uses a "prepared statement" (maybe?),  (separates query logic from data) safer against sql injection
     @Transactional
     @Modifying
     @Query("UPDATE AppUser a " +
--- src/main/java/com/jsimmons/lrnr/security/config/WebSecurityConfig.java
+++ src/main/java/com/jsimmons/lrnr/security/config/WebSecurityConfig.java
@@ -24,7 +24,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                    .antMatchers("/api/v*/registration/**", "/**")
+                    .antMatchers("/api/v*/registration/**"//, "/**"
+                              )
                     .permitAll()
                 .anyRequest()
                 .authenticated().and()
