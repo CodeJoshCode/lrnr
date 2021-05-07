18b0dad770d01d404c716a25968bb9f55f256e3b
Brandon Dehmer <brandond27@comcast.net>
2021-05-07 09:27:13 -0500

    configured email sending with amazon SES, TO addresses must be verified (SES account in sandbox mode). Credentials stored in env vars will pgp them to you josh

--- src/main/java/com/jsimmons/lrnr/email/EmailService.java
+++ src/main/java/com/jsimmons/lrnr/email/EmailService.java
@@ -3,6 +3,8 @@ package com.jsimmons.lrnr.email;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
@@ -13,13 +15,21 @@ import javax.mail.internet.MimeMessage;
 
 
 @Service
-@AllArgsConstructor
 public class EmailService implements EmailSender {
 
     private final Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
 
     private final JavaMailSender mailSender;
 
+    @Value("${EMAIL}")
+    private String fromAddress;
+
+    @Autowired
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    //TODO: verify email addresses prior to sending confirmation email
     @Override
     @Async
     public void send(String to, String email) {
@@ -29,7 +39,7 @@ public class EmailService implements EmailSender {
             helper.setText(email, true);
             helper.setTo(to);
             helper.setSubject("Confirm your Lrnr email");
-            helper.setFrom("hello@lrnr.com");
+            helper.setFrom(fromAddress);
             mailSender.send(mimeMessage);
         } catch(MessagingException e) {
             LOGGER.error("failed to send email", e);
